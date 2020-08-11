7c11300ef1626ac25f925b8a11e09d4db463c754
Nijanthan Vijayakumar <nijanthanvijayakumar@outlook.com>
2020-08-11 20:00:55 +0800

    Add function to fetch current covid cases

--- dags/utils.py
+++ dags/utils.py
@@ -8,3 +8,28 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import Variable
 
 from jinja2 import Environment, FileSystemLoader
+
+import covid19cases as covid
+
+def fetch_current_cases():
+	current_cases = covid.get_country_cases("Australia")
+	country = current_cases["CountryOrRegion"]
+	total_cases = current_cases["TotalCases"]
+	active_cases = current_cases["ActiveCases"]
+	new_cases = current_cases["NewCases"]
+	new_deaths = current_cases["NewDeaths"]
+	updated_on = current_cases["LastUpdated"]
+	time_stamp = datetime.date(datetime.now())
+	file_name = "current_cases_" + time_stamp + ".txt"
+	with open(file_name, "w+") as covid_data:
+		covid_data.write("""
+			Country: {0}
+			Total Cases: {1}
+			Active Cases: {2}
+			New Cases: {3}
+			New Deaths: {4}
+			Update on: {5}
+			""".format(country, total_cases, active_cases, new_cases, new_deaths, updated_on))
+
+
+
