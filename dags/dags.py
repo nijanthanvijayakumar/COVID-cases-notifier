eeca725b1d5f07aab5d494f2e4eddd4d7e841577
Nijanthan Vijayakumar <nijanthanvijayakumar@outlook.com>
2020-08-11 17:58:29 +0800

    Create dags skeleton code

---
+++ dags/dags.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.email_operator import EmailOperator
+from airflow.operators.python_operator import PythonOperator
+
+default_args = {
+    "owner": "me",
+    "depends_on_past": False,
+    "start_date": datetime(2019, 10, 9),
+    # "email": ["my_email@mail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1),
+    "schedule_interval": "@daily",
+}
+
+with DAG("google_news", default_args=default_args) as dag:
+	Task_I = PythonOperator(
+
+	)
+
+	Task_II = PythonOperator(
+
+	)
+
+Task_I >> Task_II
