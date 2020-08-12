7addff8f95c4904a71191e926b54f8008bcecf82
Nijanthan Vijayakumar <nijanthanvijayakumar@outlook.com>
2020-08-12 19:21:12 +0800

    Complete function to render email content

--- dags/utils.py
+++ dags/utils.py
@@ -15,3 +15,8 @@ def fetch_current_cases():
 		covid_data.write("Country\t: {0}\nTotal Cases\t: {1}\nActive Cases\t: {2}\nNew Cases\t: {3}\nNew Deaths\t: {4}\nUpdate on\t: {5}".format(current_cases["CountryOrRegion"], current_cases["TotalCases"], current_cases["ActiveCases"], current_cases["NewCases"], current_cases["NewDeaths"], current_cases["LastUpdated"]))
 
 def render_content(**context):
+	input_file = "current_cases_" + str(datetime.date(datetime.now())) + ".txt"
+	with open(file_name, "r+") as content:
+		str_content = content.read()
+		task_instance = context["task_instance"]
+		task_instance.xcom_push(key="email_content", value=email_content)
