980a877ab85c359d519d86db06d58da570d17955
Nijanthan Vijayakumar <nijanthanvijayakumar@outlook.com>
2020-08-12 22:18:45 +0800

    Update the indentation

--- dags/utils.py
+++ dags/utils.py
@@ -16,13 +16,15 @@ def fetch_current_cases():
 		covid_data.write("Country: {0}\nTotal Cases: {1}\nActive Cases: {2}\nNew Cases: {3}\nNew Deaths: {4}\nUpdate on: {5}".format(current_cases["CountryOrRegion"], current_cases["TotalCases"], current_cases["ActiveCases"], current_cases["NewCases"], current_cases["NewDeaths"], current_cases["LastUpdated"]))
 
 def render_content(**context):
+	cases_dict = {}
 	input_file = "current_cases_" + str(datetime.date(datetime.now())) + ".txt"
 	with open(input_file, "r+") as content:
 		file_content = content.read().splitlines()
 		cases_dict = {elem.split(": ")[0]: elem.split(": ")[1] for elem in file_content}
-		root = os.path.dirname(os.path.abspath(__file__))
-		env = Environment(loader=FileSystemLoader(root))
-		template = env.get_template("email_template.html")
-		str_connect = template.render(case_data=cases_dict)
-		task_instance = context["task_instance"]
-		task_instance.xcom_push(key="email_content", value=str_content)
+
+	root = os.path.dirname(os.path.abspath(__file__))
+	env = Environment(loader=FileSystemLoader(root))
+	template = env.get_template("email_template.html")
+	str_content = template.render(case_data=cases_dict)
+	task_instance = context["task_instance"]
+	task_instance.xcom_push(key="email_content", value=str_content)
