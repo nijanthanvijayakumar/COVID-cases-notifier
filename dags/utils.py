41df68b125ad100929aab6a1a592c792a3d53ded
Nijanthan Vijayakumar <nijanthanvijayakumar@outlook.com>
2020-08-12 21:56:25 +0800

    Convert to dictionary

--- dags/utils.py
+++ dags/utils.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 import requests
 import covid19cases as covid
+from jinja2 import Environment, FileSystemLoader
 
 def fetch_current_cases():
 	"""
@@ -12,11 +13,12 @@ def fetch_current_cases():
 	current_cases = covid.get_country_cases("Australia")
 	file_name = "current_cases_" + str(datetime.date(datetime.now())) + ".txt"
 	with open(file_name, "w+") as covid_data:
-		covid_data.write("Country\t: {0}\nTotal Cases\t: {1}\nActive Cases\t: {2}\nNew Cases\t: {3}\nNew Deaths\t: {4}\nUpdate on\t: {5}".format(current_cases["CountryOrRegion"], current_cases["TotalCases"], current_cases["ActiveCases"], current_cases["NewCases"], current_cases["NewDeaths"], current_cases["LastUpdated"]))
+		covid_data.write("Country: {0}\nTotal Cases: {1}\nActive Cases: {2}\nNew Cases: {3}\nNew Deaths: {4}\nUpdate on: {5}".format(current_cases["CountryOrRegion"], current_cases["TotalCases"], current_cases["ActiveCases"], current_cases["NewCases"], current_cases["NewDeaths"], current_cases["LastUpdated"]))
 
 def render_content(**context):
 	input_file = "current_cases_" + str(datetime.date(datetime.now())) + ".txt"
 	with open(input_file, "r+") as content:
-		str_content = content.read()
+		file_content = content.read().splitlines()
+		cases_dict = {elem.split(": ")[0]: elem.split(": ")[1] for elem in file_content}
 		task_instance = context["task_instance"]
 		task_instance.xcom_push(key="email_content", value=str_content)
