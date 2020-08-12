5c5e85d406f2be35475cd26137af305fd8b31b3e
Nijanthan Vijayakumar <nijanthanvijayakumar@outlook.com>
2020-08-12 23:17:59 +0800

    Update content to be passed

--- dags/utils.py
+++ dags/utils.py
@@ -22,11 +22,10 @@ def render_content(**context):
 		file_content = content.read().splitlines()
 		cases_dict = {elem.split(": ")[0]: elem.split(": ")[1] for elem in file_content}
 
-	cases_content = json.loads(json.dumps(cases_dict))
-
-	root = os.path.dirname(os.path.abspath(__file__))
-	env = Environment(loader=FileSystemLoader(root))
-	template = env.get_template("email_template.html")
-	str_content = template.render(case_data=cases_content)
+	# root = os.path.dirname(os.path.abspath(__file__))
+	# env = Environment(loader=FileSystemLoader(root))
+	# template = env.get_template("email_template.html")
+	content = f"""Country: {cases_dict['Country']}\nTotal Cases: {cases_dict['Total Cases']}\nActive Cases: {cases_dict['Active Cases']}\nNew Cases: {cases_dict['New Cases']}\nNew Deaths: {cases_dict['New Deaths']}\nUpdated on: {cases_dict['Updated on']}"""
+	str_content = template.render(case_data=content)
 	task_instance = context["task_instance"]
 	task_instance.xcom_push(key="email_content", value=str_content)
