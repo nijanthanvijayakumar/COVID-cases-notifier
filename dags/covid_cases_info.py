a92f0c65d5973a0b8912ec11fc705dfbe43d0a39
Nijanthan Vijayakumar <nijanthanvijayakumar@outlook.com>
2020-08-14 17:50:13 +0800

    Rearrange the import statements

--- dags/covid_cases_info.py
+++ dags/covid_cases_info.py
@@ -1,7 +1,7 @@
-from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.email_operator import EmailOperator
 from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
 from helper_utils import fetch_current_cases, render_content
 
 default_args = {
