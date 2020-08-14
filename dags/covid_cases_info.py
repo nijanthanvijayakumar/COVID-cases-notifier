54f57dc9bb08e4bab4e5b280a849491c10b52a87
Nijanthan Vijayakumar <nijanthanvijayakumar@outlook.com>
2020-08-14 17:49:28 +0800

    Update function name in the import statement

--- dags/covid_cases_info.py
+++ dags/covid_cases_info.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.email_operator import EmailOperator
 from airflow.operators.python_operator import PythonOperator
-from utils import fetch_current_cases, render_content
+from helper_utils import fetch_current_cases, render_content
 
 default_args = {
     "owner": "me",
